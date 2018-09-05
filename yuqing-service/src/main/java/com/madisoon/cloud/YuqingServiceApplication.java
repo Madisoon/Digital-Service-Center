ac5328fa56e8ebcd89d82ae9fb8b88e9fee7ef4d
zg <zgyxx091418@163.com>
2018-09-05 22:05:27 +0800

    微服务架构，config配置

---
+++ yuqing-service/src/main/java/com/madisoon/cloud/YuqingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.madisoon.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuqingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuqingServiceApplication.class, args);
+	}
+}
