4883361313d620eb3aaf3672409520b41b6eb379
zg <zgyxx091418@163.com>
2018-12-17 17:10:11 +0800

    微信服务提交代码

---
+++ wechat-server/src/main/java/com/madisoon/cloud/WechatServerApplication.java
@@ -0,0 +1,14 @@
+package com.madisoon.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatServerApplication.class, args);
+	}
+
+}
+
---
+++ wechat-server/src/test/java/com/madisoon/cloud/WechatServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.madisoon.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WechatServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
