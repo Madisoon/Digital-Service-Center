f6b23e5ebe071bbaf5973d433dddad01062c1a65
zg <zgyxx091418@163.com>
2018-12-17 17:08:53 +0800

    Spring 微服务框架

--- gateway-service/src/main/java/com/madisoon/cloud/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/madisoon/cloud/GatewayServiceApplication.java
@@ -3,12 +3,14 @@ package com.madisoon.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @RestController
 @SpringBootApplication
+@RefreshScope
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
